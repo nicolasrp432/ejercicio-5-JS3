8f2b8c7640352e91cc21e1d1c945001e67e0bdae
portegag2 <portegag2@gmail.com>
2025-03-21 21:13:17 +0100

    carrito.html Funcionando boton Eliminar

--- Pedro/js/Carrito.js
+++ Pedro/js/Carrito.js
@@ -18,7 +18,7 @@ export class Carrito {
     eliminarProducto(productoId) {
         this.items = this.items.filter(item => item.producto.id !== productoId);
         this.guardarCarrito();
-        this.actualizarBadge();
+        //this.actualizarBadge();
     }
 
     obtenerTotal() {
