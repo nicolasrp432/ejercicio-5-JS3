2f3c7a684a878c1bc42dc5660d389044d3257ff5
ivangago2000 <ivan.gago2000@gmail.com>
2025-03-21 12:30:07 +0100

    carrito Ivan primeros pasos

---
+++ IVAN/js/PedroEscaparate.js
@@ -0,0 +1,43 @@
+export let productos = []
+import {mostrarProductos} from '/Pedro/js/cargaProductos.js';
+import {generarMenu} from '/Pedro/js/cargaProductos.js';
+import { setupNavLinks  } from '/Pedro/js/nicolasFiltrado.js';
+
+
+/*
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("/Pedro/json/productos.json")  // Cargar el archivo JSON
+        .then(response => response.json())  // Convertir a objeto JS
+        .then(productos => mostrarProductos(productos))  // Procesar datos
+        .catch(error => console.error("Error al cargar productos:", error));
+
+        setupNavLinks();
+
+        if (productos.length === 0) {
+            //console.log("El array productos está vacío.");
+            //return;
+        } else {
+            //console.log("El array productos tiene " + productos.length + " elementos.");
+        }
+});
+*/
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("/Pedro/json/productos.json")  // Cargar el archivo JSON
+        .then(response => response.json())  // Convertir a objeto JS
+        .then(data => {
+            productos.length = 0;  // Vaciar el array sin cambiar la referencia
+            productos.push(...data);  // Asignar los datos a la variable productos
+            //console.log("Productos cargados:", productos);  // Verificar los datos cargados
+            generarMenu(productos);  // Procesar datos
+            mostrarProductos(productos);  // Procesar datos
+            setupNavLinks();
+            //console.log("Después de mostrarProductos:", productos);  // Verificar que productos mantiene su valor
+        })
+        .catch(error => console.error("Error al cargar productos:", error));
+    
+
+
+    
+
+    
+});
---
+++ IVAN/js/productos.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("productos.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("productos").innerHTML = data;
+        })
+        .catch(error => console.error("Error cargando el HTML de productos:", error));
+});
