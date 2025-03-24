7b571544e5a3c42f58cf73e574656c85da591e0e
nicolasrp432 <nicolasrp432@gmail.com>
2025-03-24 10:29:56 +0100

    cambios item carrito

--- Pedro/js/itemCarrito.js
+++ Pedro/js/itemCarrito.js
@@ -3,11 +3,13 @@ export function mostrarItem(item) {
             <div class="item-carrito">
                 <img src="/Pedro/img/imgProductos/${item.producto.imagen}" alt="${item.producto.nombre}">
                 <div class="item-detalles">
-                    <h3>${item.producto.nombre}</h3>
+                    <div class="detalles-principales">
+                        <h3>${item.producto.nombre}</h3>
                     <p>Precio: ${item.producto.precio.toFixed(2)}€</p>
                     <p>Cantidad: ${item.cantidad}</p>
                     <p>Subtotal: ${(item.producto.precio * item.cantidad).toFixed(2)}€</p>
                     <button class="btn-eliminar" data-id="${item.producto.id}">Eliminar</button>
+                    </div>
                 </div>
             </div>
         `;
