5ee864c5bd3647796ebcbe717d33b27cef618a7b
portegag2 <portegag2@gmail.com>
2025-03-25 23:46:59 +0100

    pruebas sin acertar

--- Pedro/js/imprimir.js
+++ Pedro/js/imprimir.js
@@ -3,13 +3,17 @@ import { carrito } from './vistaCarrito.js';
 import { actualizarStockComprados } from './vistaCarrito.js';
 
 document.getElementById('btn-imprimir').addEventListener('click', function () {
-        window.print(); // Abre el cuadro de diálogo de impresión
+         // Abre el cuadro de diálogo de impresión
         console.log("Imprimiendo carrito:", carrito);
         if (carrito.items.length > 0) {
-            actualizarStockComprados();
             alert('¡Gracias por tu compra!');
+            actualizarStockComprados();
+            window.print();
+            console.log("Imprimiendo carrito:", carrito);
             carrito.items = [];
+            console.log("Imprimiendo llamar a guardarCarrito:");
             carrito.guardarCarrito();
+            console.log("Imprimiendo llamar a mostrarCarrito:");
             mostrarCarrito();
         }
     });
