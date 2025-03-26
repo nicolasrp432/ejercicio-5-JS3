771153a626bcc52312f17d01a440ca1b830c86af
portegag2 <portegag2@gmail.com>
2025-03-26 11:28:39 +0100

    Entrega en estado Beta, Necesita PROBARSE

--- Entrega/js/escaparate.js
+++ Entrega/js/escaparate.js
@@ -1,7 +1,9 @@
 export let productos = []
 import {mostrarProductos, generarMenu} from './cargaProductos.js';
 import { setupNavLinks  } from './Filtrado.js';
+import { obtenerJsonCompleto } from './bbddFunciones.js';
 
+/* Codigo de cuando se cargaba desde el JSON
 document.addEventListener("DOMContentLoaded", () => {
     fetch("./json/productos.json")  // Cargar el archivo JSON desde la carpeta raíz
         .then(response => response.json())  // Convertir a objeto JS
@@ -14,3 +16,28 @@ document.addEventListener("DOMContentLoaded", () => {
         })
         .catch(error => console.error("Error al cargar productos:", error));  
 });
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+        obtenerJsonCompleto()
+        .then((jsonString) => {
+    
+            if (jsonString) {
+        
+              try {
+        
+                productos = JSON.parse(jsonString); // Convierte la cadena JSON a un objeto
+        
+                // Ahora 'productos' debería ser un array de objetos
+                generarMenu(productos);
+                setupNavLinks();
+                mostrarProductos(productos); // Llama a tu función mostrarProductos        
+              } catch (error) {        
+                console.error("Error al analizar la cadena JSON:", error);        
+              }        
+            } else {        
+              console.error("No se pudo obtener el JSON de la colección.");       
+
+            }    
+        });
+  });
